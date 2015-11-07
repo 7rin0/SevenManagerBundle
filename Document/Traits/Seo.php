da918e842f18023c4512f1df4348d4378ada0dbf
Luis SEVERINO <lfs.severino@gmail.com>
2015-11-07 15:30:58 +0100

    seo

--- Document/Traits/Seo.php
+++ Document/Traits/Seo.php
@@ -3,11 +3,15 @@
 namespace SevenManagerBundle\Document\Traits;
 
 use Symfony\Cmf\Bundle\SeoBundle\Doctrine\Phpcr\SeoMetadata;
-use Doctrine\ODM\PHPCR\Mapping\Annotations as PHPCRODM;
+use Doctrine\ODM\PHPCR\Mapping\Annotations as PHPCR;
 
+/**
+ * Class Seo
+ *
+ * @package SevenManagerBundle\Document\Traits
+ */
 trait Seo
 {
-
     public function __construct()
     {
         $this->__constructSeoMetaData();
@@ -16,7 +20,7 @@ trait Seo
     /**
      * @var SeoMetadata
      *
-     * @PHPCRODM\Child
+     * @PHPCR\Child
      */
     protected $seoMetadata;
 
