f78215ffd7208172bb15f4ee93d478c3d0ca8468
Luis SEVERINO <lfs.severino@gmail.com>
2015-12-15 23:05:13 +0100

    TitleImage document

---
+++ Document/Blocks/TitleImage.php
@@ -0,0 +1,43 @@
+<?php
+/**
+ * User: lseverino
+ * Date: 20/10/15
+ * Time: 14:02
+ */
+
+namespace SevenManagerBundle\Document\Blocks;
+
+use SevenManagerBundle\Document\Traits\Fields\HTML\Images;
+use SevenManagerBundle\Document\Traits\Fields\HTML\Texts;
+use Symfony\Cmf\Bundle\BlockBundle\Doctrine\Phpcr\AbstractBlock;
+use Symfony\Cmf\Bundle\CoreBundle\Translatable\TranslatableInterface;
+use Doctrine\ODM\PHPCR\Mapping\Annotations as PHPCR;
+
+/**
+ * Class TitleImage
+ * @PHPCR\Document(referenceable=true, translator="attribute")
+ *
+ * @package SevenManagerBundle\Document\Blocks
+ */
+class TitleImage extends AbstractBlock implements TranslatableInterface
+{
+    use Texts, Images;
+
+    /**
+     * @PHPCR\Locale()
+     */
+    protected $locale;
+
+    /**
+     * @PHPCR\ReferenceMany(cascade="all")
+     */
+    protected $children;
+
+    /**
+     * @return string
+     */
+    public function getType()
+    {
+        return 'seven_manager.admin.blocks.title.image';
+    }
+}
